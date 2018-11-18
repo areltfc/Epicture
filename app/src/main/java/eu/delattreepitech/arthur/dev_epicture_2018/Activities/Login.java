212b3abac38170300d7c13b2463e3936a1c3cb3b
Arthur Delattre <arthur.delattre@epitech.eu>
2018-11-18 10:30:07 +0100

    Fixed tokens storage. Now, when tokens are found in the SharedPreferences, their validity is tested with an API call. Also fixed a typo.

--- app/src/main/java/eu/delattreepitech/arthur/dev_epicture_2018/Activities/Login.java
+++ app/src/main/java/eu/delattreepitech/arthur/dev_epicture_2018/Activities/Login.java
@@ -11,9 +11,14 @@ import android.widget.Toast;
 
 import com.google.gson.Gson;
 
+import java.io.IOException;
+
 import eu.delattreepitech.arthur.dev_epicture_2018.R;
 import eu.delattreepitech.arthur.dev_epicture_2018.Types.User;
 import eu.delattreepitech.arthur.dev_epicture_2018.WebViews.AuthWebViewClient;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 public class Login extends AppCompatActivity {
     final static String CLIENT_ID = "7f1c902a0216035";
@@ -23,35 +28,53 @@ public class Login extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         StrictMode.setThreadPolicy(new StrictMode.ThreadPolicy.Builder().permitAll().build());
 
-        lookForExistingTokens();
-
-        Intent deniedCheck = getIntent();
-        if (deniedCheck != null && deniedCheck.getBooleanExtra("denied", false)) {
-            Toast.makeText(getApplicationContext(), "Please grant us access to your account", Toast.LENGTH_SHORT).show();
-        }
-        try {
+        if (!lookForExistingTokens()) {
+            Intent deniedCheck = getIntent();
+            if (deniedCheck != null && deniedCheck.getBooleanExtra("denied", false)) {
+                Toast.makeText(getApplicationContext(), "Please grant us access to your account", Toast.LENGTH_SHORT).show();
+            }
             this.redirectLogin();
-        } catch (Exception e) {
-            e.printStackTrace();
         }
     }
 
-    void lookForExistingTokens() {
+    boolean lookForExistingTokens() {
         final SharedPreferences sharedPref = getSharedPreferences(getString(R.string.shared_preferences_file), Context.MODE_PRIVATE);
         final String tokens = sharedPref.getString("tokens", null);
 
         if (tokens != null) {
             final Intent home = new Intent(getApplicationContext(), Home.class);
-            home.putExtra("user", new Gson().toJson(new User(tokens)));
-            startActivity(home);
+            final User user = new User(tokens);
+            if (tokensAreValid(user)) {
+                home.putExtra("user", new Gson().toJson(user));
+                startActivity(home);
+                return true;
+            }
         }
+        return false;
+    }
+
+    boolean tokensAreValid(final User user) {
+        OkHttpClient client = new OkHttpClient.Builder().build();
+        Request request = new Request.Builder()
+                .url("https://api.imgur.com/3/account/" + user.getAccountUsername())
+                .addHeader("Authorization", "Bearer " + user.getAccessToken())
+                .build();
+        try {
+            Response response = client.newCall(request).execute();
+            if (!response.isSuccessful()) {
+                return false;
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return true;
     }
 
     void redirectLogin() {
         try {
             WebView view = new WebView(this);
             view.setWebViewClient(new AuthWebViewClient(this));
-            view.loadUrl("https://api.imgur.com/oauth2/authorize?client_id= " + CLIENT_ID + "&response_type=token");
+            view.loadUrl("https://api.imgur.com/oauth2/authorize?client_id=" + CLIENT_ID + "&response_type=token");
             setContentView(view);
         } catch (Exception e) {
             e.printStackTrace();
